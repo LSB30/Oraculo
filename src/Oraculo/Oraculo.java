6b08329d4280b12694ece630742e429a86495a60
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-09-04 15:17:12 -0300

    ✅ done

---
+++ src/Oraculo/Oraculo.java
@@ -0,0 +1,10 @@
+package Oraculo;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Oraculo");
+    }
+}
